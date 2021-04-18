16bb109e087bda3a6b224189feb50cf5b4180470
VeenaSank <goldlog.veena@gmail.com>
2021-04-18 20:35:48 +1000

    made it better

---
+++ src/useUserFetcher.js
@@ -0,0 +1,41 @@
+import { useState, useEffect, useCallback } from "react";
+
+const API_URL =
+  "https://gist.githubusercontent.com/medibank-digital/a1fc81a93200a7b9d5f8b7eae0fac6f8/raw/de10a4fcf717e6c431e88c965072c784808fd6b2/people.json";
+
+export  const useUserFetcher = () => {
+    const [userData, setUserData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
+  
+    const fetchData =  useCallback( () => {
+        setLoading(true)
+        // This setTimeout exists to show the loading indicator
+        setTimeout(
+          async () =>
+            await fetch(API_URL)
+              .then((res) => (res.text !== "" ? res.json() : {}))
+              .then((data) => {
+               successFetchHandler(data)
+              })
+              .catch((err) => console.log("Error in fetching", failFetchHandler(err))),
+    
+          2000
+        );
+      }, [] );
+      
+    let successFetchHandler = (data) => {
+        setLoading(false)
+        setUserData(data)
+    };
+  
+    let failFetchHandler = (error) => setError(error);
+  
+    useEffect(() => {       
+        fetchData();
+    }, [ fetchData]);
+  
+   
+    return [loading, userData, error];
+  };
+  
